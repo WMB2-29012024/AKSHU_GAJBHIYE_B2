ff8bd0d1fb24959ac82363c8a8d3132c722c6e2d
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-21 17:34:48 +0530

    W4_D2_Q2: log the first and 5th element of the array

---
+++ question2.js
@@ -0,0 +1,7 @@
+//Log the first and 5th element of the array/list.
+
+const primitieDataType = ["String", "number", "boolean", "null", "undefined", "symbol", "bigint"];
+console.log(primitieDataType);
+
+
+console.log(primitieDataType[0], primitieDataType[4]);
