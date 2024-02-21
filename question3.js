326392828203be3016d8ec36f8df4bdedc7ff6eb
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-21 18:24:51 +0530

    W4_D2_Q3 : Log the last element of the array/list of length 6.

---
+++ question3.js
@@ -0,0 +1,5 @@
+//Log the first and 5th element of the array/list.
+
+const primitieDataType = ["String", "number", "boolean", "null", "undefined", "symbol", "bigint"];
+
+console.log(primitieDataType[primitieDataType.length-1]);
