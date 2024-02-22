dd4d27e2c82d5f321b1820de90b5dd127cbaec4d
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:12:56 +0530

    Use shift method to remove first element in the array, and log the removed element. Also log the length of array after removal of element.

---
+++ question7.js
@@ -0,0 +1,7 @@
+//Use shift method to remove first element in the array, and log the removed element. Also log the length of array after removal of element.
+
+let primitieDataType = ["String", "number", "boolean", "null", "undefined", "symbol", "bigint"];
+
+let a=primitieDataType.shift();
+console.log(a);
+console.log(primitieDataType.length);
