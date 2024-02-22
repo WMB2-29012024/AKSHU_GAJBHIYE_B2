3e0b3f4ea1c7e89759db4ac865e8190b382d7836
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:38:55 +0530

    create an object with key primitivetypes and value should be an array of strings containing names of all primitive data types

---
+++ question17.js
@@ -0,0 +1,9 @@
+//Create an object with key “primitivetypes”, and value should be an array of strings containing names of all primitive data types in JS
+
+let obj = {
+    "primitivetypes": [
+        "String", "number", "boolean", "null", "undefined", "symbol", "bigint"
+    ]
+}
+
+console.log(obj);
