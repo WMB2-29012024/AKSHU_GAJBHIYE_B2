281baa6fd9e6b0e75f9278bff679e35685de025e
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 22:50:41 +0530

    print the value and with the left side value also

---
+++ question9.js
@@ -0,0 +1,26 @@
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function abc(arr) {
+    const a = [];
+    for (let i = 0; i <= arr.length - 1; i = i + 1) {
+        if (i === 0) {
+            a.push([arr[i]])
+        } else {
+            a.push([arr[i], arr[i - 1]]);
+        }
+
+    }
+
+    return a;
+}
+
+console.log(abc(numArr));
+
+
+// [
+//     [ 1 ],    [ 2, 1 ],
+//     [ 3, 2 ], [ 4, 3 ],
+//     [ 5, 4 ], [ 6, 5 ],
+//     [ 7, 6 ], [ 8, 7 ],
+//     [ 9, 8 ], [ 10, 9 ]
+//   ]
