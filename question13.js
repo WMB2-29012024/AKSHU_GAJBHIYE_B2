003db137007fea0243c19a6e8cdd7169a84f6d29
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:29:47 +0530

    Log the vale of key phone in the object

---
+++ question13.js
@@ -0,0 +1,21 @@
+//Log the value of key “phone” in the object (square notation, dot notation any).
+
+
+let obj = {
+    "name": "Akshu",
+    "age": 23,
+    "email": "akshugajbhiye97@gmail.com",
+    "phone": "7***4413*2",
+    "address": "bhopal",
+    "pincode":262004
+}
+
+let phone=obj.phone;
+
+console.log(phone);
+
+
+
+
+
+
