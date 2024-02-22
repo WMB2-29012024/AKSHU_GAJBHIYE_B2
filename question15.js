f645f30dad76b573d88ddd9da06e3d250b1c890c
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:32:27 +0530

    Delete the key email with its value from object

---
+++ question15.js
@@ -0,0 +1,20 @@
+//Delete the key “email” with its value from object, and log the object (square notation, dot notation any)
+
+
+let obj = {
+    "name": "Akshu",
+    "age": 23,
+    "email": "akshugajbhiye97@gmail.com",
+    "phone": "7***4413*2",
+    "address": "bhopal",
+    "pincode":262004
+}
+
+delete obj.email;
+
+console.log(obj);
+
+
+
+
+
