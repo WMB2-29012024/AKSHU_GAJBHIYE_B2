650046cb5a92a2ac0726112499a091a5facd0caf
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:07:11 +0530

    Use unshift method to add new data in the array, and log the new length of the array, but without using the .length property.

---
+++ question6.js
@@ -0,0 +1,6 @@
+//Use unshift method to add new data in the array, and log the new length of the array, but without using the .length property.
+
+let primitieDataType = ["String", "number", "boolean", "null", "undefined", "symbol", "bigint"];
+
+let a=primitieDataType.unshift("akshu");
+console.log(a);
