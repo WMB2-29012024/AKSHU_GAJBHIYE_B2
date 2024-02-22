54c6b45da35a31772d5742f6f6076e3cb1325ec7
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:28:20 +0530

    Log the keys from the object in an array, using Object.keys.

---
+++ question12.js
@@ -0,0 +1,17 @@
+//Log the keys from the object in an array, using Object.keys.
+
+
+let obj = {
+    "name": "Akshu",
+    "age": 23,
+    "email": "akshugajbhiye97@gmail.com",
+    "phone": 7999441302,
+    "address": "bhopal",
+    "pincode":262004
+}
+
+let keys=Object.keys(obj)
+console.log(keys);
+
+
+
