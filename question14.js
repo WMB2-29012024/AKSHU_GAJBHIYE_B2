cab0ecbc8ac53dacb401a4ab798c7396c09331e4
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:31:05 +0530

    Change the value of key pincode and log the new value

---
+++ question14.js
@@ -0,0 +1,21 @@
+//Change the value of key “pincode” and log the new value (square notation, dot notation any).
+
+
+let obj = {
+    "name": "Akshu",
+    "age": 23,
+    "email": "akshugajbhiye97@gmail.com",
+    "phone": "7***4413*2",
+    "address": "bhopal",
+    "pincode":262004
+}
+
+let a=obj.pincode;
+
+a=262000;
+
+console.log(a);
+
+
+
+
