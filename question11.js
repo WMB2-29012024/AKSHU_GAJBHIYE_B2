a0cb30a0d0743b96aedff445f589e5ad2d2c9f11
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:25:02 +0530

    Log the values from the object in an array, using Object.values.

---
+++ question11.js
@@ -0,0 +1,17 @@
+//Log the values from the object in an array, using Object.values.
+
+
+let obj = {
+    "name": "Akshu",
+    "age": 23,
+    "email": "akshugajbhiye97@gmail.com",
+    "phone": 7999441302,
+    "address": "bhopal",
+    "pincode":262004
+}
+
+let value=Object.values(obj)
+console.log(value);
+
+
+
