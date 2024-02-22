670c9ba42443df472b28645793d21bc2e40375f9
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:51:13 +0530

    create an array of objects , where each object represents a primitive data type

---
+++ question20.js
@@ -0,0 +1,36 @@
+//Create an array of objects, where each object represents a primitive data type in JS, its keys should be like Eg: {“dataTypeName”: “string”, “type”: “primitive”, “isObject”: false, “isMultipleValuesAllowed”: false}
+
+let primitiveDataTypes = [
+    {
+        dataTypeName: 'string',
+        type: 'primitive',
+        isObject: false,
+        isMultipleValuesAllowed: false
+    },
+    {
+        dataTypeName: 'number',
+        type: 'primitive',
+        isObject: false,
+        isMultipleValuesAllowed: false
+    },
+    {
+        dataTypeName: 'boolean',
+        type: 'primitive',
+        isObject: false,
+        isMultipleValuesAllowed: false
+    },
+    {
+        dataTypeName: 'undefined',
+        type: 'primitive',
+        isObject: false,
+        isMultipleValuesAllowed: false
+    },
+    {
+        dataTypeName: 'null',
+        type: 'primitive',
+        isObject: false,
+        isMultipleValuesAllowed: false
+    }
+];
+
+console.log(primitiveDataTypes);
