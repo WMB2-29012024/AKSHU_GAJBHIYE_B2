02c74b476e0e6de4906f0fda781912f3ab98fee8
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:19:57 +0530

    Create an object with keys

---
+++ question10.js
@@ -0,0 +1,13 @@
+//Create an object with following keys = “name”, “age”, “email”, “phone”, “address”, “pincode”. And add their corresponding values, and log the object.
+
+
+let obj = {
+    "name": "Akshu",
+    "age": 23,
+    "email": "akshugajbhiye97@gmail.com",
+    "phone": 7999441302,
+    "address": "bhopal",
+    "pincode":262004
+}
+
+console.log(obj);
