a8509330aa9cea5ce2bd8a50ef1b55532f7a6165
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 14:01:59 +0530

    Use pop method to remove last element in the array, and log the removed element. Also log the length after removal of element.

---
+++ question5.js
@@ -0,0 +1,7 @@
+//Use pop method to remove last element in the array, and log the removed element. Also log the length after removal of element.
+
+let primitieDataType = ["String", "number", "boolean", "null", "undefined", "symbol", "bigint"];
+
+let a=primitieDataType.pop();
+
+console.log(a);
