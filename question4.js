bdbbd0a9720dc9b648093e9b6083e669d9a50da5
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-22 13:58:20 +0530

    Use push method to add new data in the array, and log the new length of the array, but without using the .length property.

---
+++ question4.js
@@ -0,0 +1,6 @@
+//Use push method to add new data in the array, and log the new length of the array, but without using the .length property.
+
+const primitieDataType = ["String", "number", "boolean", "null", "undefined", "symbol", "bigint"];
+
+let a = primitieDataType.push("akshu");
+console.log(a);
