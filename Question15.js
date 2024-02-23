a2510b6db99b84405f52c45d8567df7b3829528a
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-23 19:29:30 +0530

    There are 2 different arrays, write a for-loop, that: // starts from 0th index, on array one,

--- Question15.js
+++ Question15.js
@@ -1,11 +1,38 @@
 // Branch Name: B2_W4_D3
 
 //  Question 15: There are 2 different arrays, write a for-loop, that:
-starts from 0th index, on array one,
-matches all the elements of the array two,
-if any element matches, logs true, and continue to next element
-once all the elements are looped on array 2, go to next element in array 1, then continue from step 1.
+// starts from 0th index, on array one,
+// matches all the elements of the array two,
+// if any element matches, logs true, and continue to next element
+// once all the elements are looped on array 2, go to next element in array 1, then continue from step 1.
 
-const numArr1 = [1, 2, 3, 4, 5]
-const numArr2 = [6, 7, 8, 9, 10, 11, 12]    
+const numArr1 = [1, 2, 3, 4, 5, 10, 11]
+const numArr2 = [6, 7, 8, 9, 10, 11, 12]
 
+
+function arrrayy(arr1, arr2) {
+    let a = arr1.length;
+    let b = arr2.length;
+
+    if (a > b) {
+        for (let i = 0; i < arr1.length; i++) {
+            for (let j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    console.log(true);
+                }
+            }
+        }
+    } else {
+        for (let i = 0; i < arr1.length; i++) {
+            for (let j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    console.log(true);
+                }
+            }
+        }
+    }
+
+}
+
+
+arrrayy(numArr1, numArr2)
