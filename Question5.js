b4dd565694d8d13023b493dba26d537fb3a1039f
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-23 18:52:50 +0530

    Question 5: Write a for-loop to log every alternate element, starting from last index

--- Question5.js
+++ Question5.js
@@ -2,5 +2,16 @@
 
 //  Question 5: Write a for-loop to log every alternate element, starting from last index.
 
-const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]    
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+function even(arr) {
+    let abc = [];
+    for (let i = arr.length - 1; i > 0; i = i - 2) {
+        abc.push(arr[i]);
+    }
+    return abc;
+}
+console.log(even(numArr));
 
