903549c76a67e06f1fcfd6a90b1e5e02861b5a78
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-23 18:56:05 +0530

    Write a for-loop to log every 4th element, starting from last index

--- Question7.js
+++ Question7.js
@@ -2,5 +2,16 @@
 
 //  Question 7: Write a for-loop to log every 4th element, starting from last index
 
-const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]    
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+function even(arr) {
+    let abc = [];
+    for (let i = arr.length - 1; i > 0; i = i - 4) {
+        abc.push(arr[i]);
+    }
+    return abc;
+}
+console.log(even(numArr)); //[ 10, 6, 2 ]
 
