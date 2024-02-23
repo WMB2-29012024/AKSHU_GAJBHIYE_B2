8e3787edf83e64c99204aee71f61638e7a8aba36
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-23 18:48:41 +0530

    Write a for-loop to reverse a string.

--- Question3.js
+++ Question3.js
@@ -4,3 +4,13 @@
 
 const strArr = "asdasdzxczxc";
 
+function rev(arr) {
+    let ab = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        ab.push(arr[i]);
+    }
+    let b=ab.join('')
+    return b;
+}
+console.log(rev(strArr));
+
