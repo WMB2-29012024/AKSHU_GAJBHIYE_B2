5304e24a27c867eafdfca481b82a07ddb018e7f9
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 19:08:29 +0530

    Write a for loop to find the factorial of a given number.

--- Question18.js
+++ Question18.js
@@ -1,7 +1,19 @@
 // Branch Name: B2_W4_D5
-      
+
 /* Question 18: Write a for loop to find the factorial of a given number.
 */
 
 const num = 5;
 
+function fartorial(n) {
+    let fact = 1;
+    for (let i = n; i > 0; i--) {
+        fact *= i;
+    }
+    return fact
+}
+
+console.log(fartorial(num));
+
+
+//Result 120
