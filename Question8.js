a367b35f357b59b45885690e3313d8d9f863f1aa
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 17:44:16 +0530

    Write a for loop to log only odd numbers in the array.

--- Question8.js
+++ Question8.js
@@ -3,5 +3,16 @@
 /* Question 8: Write a for loop to log only odd numbers in the array.
 */
 
-const numArr = [10, 2, 9, 8, 6, 12, 20, 30, 4, 50];
+const numArr = [10,1,3,5, 2, 9, 8, 6, 12, 20, 30, 4, 50];
 
+function eveArray(arr) {
+    let evenarr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 1) {
+            evenarr.push(arr[i]);
+        }
+    }
+    return evenarr;
+}
+
+console.log(eveArray(numArr));
