b67f9a5f3085ac8169b3ddcf9a9410d09ed6b130
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 18:42:11 +0530

    Write a for loop to find the index of a specific element in the array.

--- Question16.js
+++ Question16.js
@@ -6,3 +6,15 @@
 const arr = ["apple", "banana", "orange", "grape"];
 const target = "orange";
 
+function arreyyy(arr,tar) {
+    let ill=0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]===tar) {
+            ill=i;
+        }
+        
+    }
+    return ill;
+}
+console.log(arreyyy(arr,target));
+
