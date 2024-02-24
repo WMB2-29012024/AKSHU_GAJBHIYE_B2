f755682c1fecc283ac63f7388918df1efb37b5ef
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 18:46:23 +0530

    Write a for loop to check if all elements in the array are positive numbers.

--- Question17.js
+++ Question17.js
@@ -5,3 +5,16 @@
 
 const arr = [10, 20, -5, 30, 40];
 
+function arrreyy(arr) {
+    let abc=[];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]>0) {
+            abc.push(arr[i])
+        }
+    }
+    return abc;
+}
+
+console.log(arrreyy(arr));
+
+//Result [ 10, 20, 30, 40 ]
