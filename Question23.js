8354a4a37d7c66e4c92daad4d1d25397035f7753
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 20:19:06 +0530

    Write a for loop to find the median of elements in an array.

--- Question23.js
+++ Question23.js
@@ -1,7 +1,19 @@
 // Branch Name: B2_W4_D5
-      
+
 /* Question 23: Write a for loop to find the median of elements in an array.
 */
 
-const arr = [1, 3, 2, 4, 5, 6];
+const arr = [2, 4, 8, 6, 9];
 
+function arrreyy(abc) {
+    let a = arr[0]
+    abc.sort();
+    for (let i = 0; i < abc.length / 2; i++) {
+        a = arr[i]
+    }
+    return a;
+}
+
+console.log(arrreyy(arr));
+
+//Result middle number is 6;
