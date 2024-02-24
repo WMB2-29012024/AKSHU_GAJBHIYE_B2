f89674077f28e6988921c89fe3b0a222e59be343
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 17:51:13 +0530

    Question 9: Write a for loop to log elements greater than 3.

--- Question9.js
+++ Question9.js
@@ -1,7 +1,22 @@
 // Branch Name: B2_W4_D5
-      
+
 /* Question 9: Write a for loop to log elements greater than 3.
 */
 
-const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numArr = [1, 2, 9, 5, 4, 4, 5, 6, 7, 8, 9];
 
+function greatArray(arr) {
+    let a = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 3) {
+            a.push(arr[i]);
+        }
+    }
+    return a;
+}
+console.log(greatArray(numArr));
+
+//Result 
+
+// [9, 5, 4, 4, 5,
+//     6, 7, 8, 9]
