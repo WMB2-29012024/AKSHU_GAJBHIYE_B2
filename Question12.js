10124e400d7a9c6acb082cc587cda22d20d08519
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 18:10:56 +0530

    Write a for loop to log elements not equal to apple

--- Question12.js
+++ Question12.js
@@ -1,7 +1,20 @@
 // Branch Name: B2_W4_D5
-      
+
 /* Question 12: Write a for loop to log elements not equal to "apple"
 */
 
 const strArr = ["banana", "apple", "orange", "grape", "kiwi"];
 
+function eqalarr(arr) {
+    let a = strArr[1];
+    let abc = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== a) {
+            abc.push(arr[i]);
+        }
+    }
+    return abc;
+}
+console.log(eqalarr(strArr));
+
+//Result [ 'banana', 'orange', 'grape', 'kiwi' ]
