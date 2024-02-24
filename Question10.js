74e1d44c449b91c3f8963ef871247bccc66ce588
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 17:55:56 +0530

    Write a for loop to log elements less than or equal to 3.

--- Question10.js
+++ Question10.js
@@ -1,7 +1,19 @@
 // Branch Name: B2_W4_D5
-      
+
 /* Question 10: Write a for loop to log elements less than or equal to 3.
 */
 
 const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+function greatArray(arr) {
+    let a = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 3) {
+            a.push(arr[i]);
+        }
+    }
+    return a;
+}
+console.log(greatArray(numArr));
+
+//Result [ 1, 2 ]
