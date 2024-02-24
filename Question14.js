b88958c725a4e5d128974e1cef2977beeb799f2a
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 18:30:00 +0530

    Write a for loop to log elements that start with the letter a

--- Question14.js
+++ Question14.js
@@ -1,7 +1,20 @@
 // Branch Name: B2_W4_D5
-      
+
 /* Question 14: Write a for loop to log elements that start with the letter "a".
 */
 
 const strArr = ["banana", "apple", "orange", "grape", "kiwi"];
 
+function arreeyy(arr) {
+    let abc = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].charAt(0).toLowerCase() == 'a') {
+            abc.push(arr[i]);
+        }
+    }
+    return abc;
+}
+
+console.log(arreeyy(strArr));
+
+//Result [ 'apple' ]
