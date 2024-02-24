60bc14a99da51c4c3ab7d0a8c858a794c99c007c
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 17:41:43 +0530

    Write a for loop to log only even numbers in the array.

--- Question7.js
+++ Question7.js
@@ -1,7 +1,25 @@
 // Branch Name: B2_W4_D5
-      
+
 /* Question 7: Write a for loop to log only even numbers in the array.
 */
 
-const numArr = [10, 2, 9, 8, 6, 12, 20, 30, 4, 50];
+const numArr = [10, 2, 9, 1, 3, 5, 8, 6, 12, 20, 30, 4, 50];
 
+function eveArray(arr) {
+    let evenarr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            evenarr.push(arr[i]);
+        }
+    }
+    return evenarr;
+}
+
+console.log(eveArray(numArr));
+
+
+// Result'
+//[
+//     10,  2, 8,  6, 12,
+//     20, 30, 4, 50
+//   ]
