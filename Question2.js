6d2bef979c93022a43783301fc0528c733e1f6b2
akshu2626 <akshugajbhiye97@gmail.com>
2024-02-24 14:55:40 +0530

    Write a for loop to log the sum of all elements.

--- Question2.js
+++ Question2.js
@@ -5,3 +5,15 @@
 
 const numArr = [1, 2, 3, 4, 5];
 
+function arreySum(arr) {
+    let sum=0;
+    for (let i = 0; i < arr.length; i++) {
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+console.log(arreySum(numArr));
+
+
+//Result 15
