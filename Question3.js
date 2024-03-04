db98edeb9baf3fc97bbaa8639336360f82349935
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-04 21:14:39 +0530

    the number formatted to have exactly two digits after the decimal point using the .toFixed() method

--- Question3.js
+++ Question3.js
@@ -1,9 +1,11 @@
 // Branch Name: B2_W6_D1
-      
+
 /* Question 3: Write a function that takes a number as input and returns the number formatted to have exactly two digits after the decimal point using the .toFixed() method.
 */
 
 function formatToTwoDigits(num) {
   // Write your code here
+  return num.toFixed(2);
 }
 
+console.log(formatToTwoDigits(2.898989));
