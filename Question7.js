8e65483e4db508c1dbef925d3a9dee14f793130a
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-05 14:45:42 +0530

    check and log if the value 10 is an integer using the .isInteger() method.

--- Question7.js
+++ Question7.js
@@ -1,6 +1,12 @@
 // Branch Name: B2_W6_D1
-      
+
 /* Question 7: check and log if the value 10 is an integer using the .isInteger() method.
 */
 
+
+let num = 12;
+
+if (Number.isInteger(num))
+    console.log("integer");
+else console.log('not integer');
 
