f8a063475e4c178bc83c9ae465176e809f706d16
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-05 15:12:02 +0530

    Check if the value 2589 is not a number (NaN) using the isNaN() method.

--- Question9.js
+++ Question9.js
@@ -1,6 +1,9 @@
 // Branch Name: B2_W6_D1
-      
+
 /* Question 9: Check if the value 2589 is not a number (NaN) using the isNaN() method.
 */
 
+let value=2589;
+if (isNaN(value)) console.log("not integer");
+    else console.log('integer');
 
