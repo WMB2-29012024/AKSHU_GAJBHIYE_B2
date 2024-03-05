46c71d48446b4e65285dac2a0ae4802c63adc2e3
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-05 14:22:39 +0530

    Convert and log the string 3.14 to a floating-point number using the parseFloat() method.

--- Question5.js
+++ Question5.js
@@ -1,6 +1,10 @@
 // Branch Name: B2_W6_D1
-      
+
 /* Question 5: Convert and log the string "3.14" to a floating-point number using the parseFloat() method.
 */
 
+let string ="3.14";
 
+let a=parseFloat(string);
+
+console.log(a);
