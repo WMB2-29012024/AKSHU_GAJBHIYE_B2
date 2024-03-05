659a1df48709e2387e2b694c3333a5325fd3078a
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-05 14:56:07 +0530

    Check and log if the value 'hello' is not a number (NaN) using the isNaN() method.

--- Question8.js
+++ Question8.js
@@ -3,4 +3,8 @@
 /* Question 8: Check and log if the value "hello" is not a number (NaN) using the isNaN() method.
 */
 
+let value="hello";
+
+if (isNaN(value)) console.log('is not a number');
+else console.log('number it is');
 
