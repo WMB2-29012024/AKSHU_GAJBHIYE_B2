7b54f9504c44bf301f5e6c97a27e2c1f37c13aef
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-05 15:21:26 +0530

    Check and log, if the value 9007199254740991 is a safe integer in JavaScript using Number.isSafeInteger()

--- Question10.js
+++ Question10.js
@@ -1,6 +1,9 @@
 // Branch Name: B2_W6_D1
-      
+
 /* Question 10: Check and log, if the value 9007199254740991 is a safe integer in JavaScript using Number.isSafeInteger().
 */
 
+let value = 9007199254740991;
 
+if (Number.isSafeInteger(value)) console.log('safe');
+else console.log('not safe');
