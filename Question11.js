448b08736f3abf57eeae37371a9f134755de24b7
akshu2626 <akshugajbhiye97@gmail.com>
2024-03-06 15:49:23 +0530

    Log the maximum safe integer value in JavaScript by using Number.MAX_SAFE_INTEGER method

--- Question11.js
+++ Question11.js
@@ -1,6 +1,18 @@
 // Branch Name: B2_W6_D1
-      
+
 /* Question 11: Log the maximum safe integer value in JavaScript by using Number.MAX_SAFE_INTEGER method
 */
 
+let arr=[1,2,3,4,5,6,7];
 
+function MyFiltere(arr,fn) {
+    let abc=[];
+    fn=()=>{
+        for (let i = 0; i < arr.length; i++) {
+            abc.push(arr[i]);
+        }
+    }
+    return abc;
+}
+
+console.log(MyFiltere(arr,));
