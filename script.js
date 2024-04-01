666348fc1da4666504077dfb047d2d82474e0ae2
akshu2626 <akshugajbhiye97@gmail.com>
2024-04-01 19:10:19 +0530

    task commpleted

---
+++ script.js
@@ -0,0 +1,18 @@
+const label = document.querySelector('label');
+const h2 = document.querySelector('h2');
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+const form=document.querySelector('form');
+
+
+
+input.addEventListener('change', (e) => {
+    e.preventDefault();
+    let a = e.target.value;
+    btn.addEventListener('click', (e) => {
+        e.preventDefault(); 
+        h2.innerHTML = 'Hi ' + a;
+    })
+})
+
+
